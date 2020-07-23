addeb614359bed9775efb3d7654b04b321f90b81
unknown <mhudyx@gmail.com>
2020-07-23 21:54:41 +0200

    Added route for homepage

--- src/layouts/Content/Content.js
+++ src/layouts/Content/Content.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './Content.scss';
+
+import { HomePage } from '../../pages';
 
 const Content = () => {
     return (
         <main>
-            MainContent
+            <Switch>
+                <Route path="/" exact component={HomePage} />
+            </Switch>
         </main>
     )
 }
