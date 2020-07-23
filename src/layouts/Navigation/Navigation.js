cbef2754b16697c7ffb77a2b856261eed3d2fd90
unknown <mhudyx@gmail.com>
2020-07-23 21:54:01 +0200

    Added links in navigation

--- src/layouts/Navigation/Navigation.js
+++ src/layouts/Navigation/Navigation.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Navigation.scss';
 
+const navList = [
+    {name: "Home", path: "/", exact: true},
+    {name: "European Masters", path: "/eumasters"},
+    {name: "Regional Leagues", path: "/leagues"},
+    {name: "Teams", path: "/teams"}
+];
+
 const Navigation = () => {
+
+    const menu = navList.map(item => (
+        <NavLink to={item.path} exact={item.exact ? item.exact : false} key={item.name}>{item.name}</NavLink>
+    ))
+
     return (
         <nav>
-            navigation
+            <div className="nav__logo"></div>
+            <div className="nav__navbar">
+                {menu}
+            </div>
         </nav>
     )
 }
