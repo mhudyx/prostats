a0f36e1e9bb8640af84924d711a3a6d295172f2f
unknown <mhudyx@gmail.com>
2020-08-25 19:14:49 +0200

    Add and initial configuration for express

---
+++ server/data.js
@@ -0,0 +1,16 @@
+export default {
+    leagues: [
+        {
+            _id: 1,
+            name: 'European Masters'
+        },
+        {
+            _id: 2,
+            name: 'Spanish League'
+        },
+        {
+            _id: 3,
+            name: 'Polish League'
+        }
+    ]
+}
---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import data from './data';
+import dotenv from 'dotenv';
+import config from './config';
+import mongoose from 'mongoose';
+import userRoute from './routes/userRoute';
+
+dotenv.config();
+const mongodbUrl = config.MONGODB_URL;
+
+mongoose.connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).catch(error => console.log(error.reason));
+
+const app = express();
+
+app.use("/api/users", userRoute);
+
+app.get('/api/leagues', (req, res) => {
+    
+    res.send(data.leagues);
+    })
+
+app.listen(3001, () => { console.log("Server is running at at http://localhost:3001") })
