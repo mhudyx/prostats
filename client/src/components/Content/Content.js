f0f8b20a679919e8ccf720612c1bfb18c826860a
unknown <mhudyx@gmail.com>
2020-08-29 11:06:59 +0200

    Add EuMasters page

--- client/src/components/Content/Content.js
+++ client/src/components/Content/Content.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { NavLink, Route, Switch } from 'react-router-dom';
 import './Content.scss';
 
-import { HomePage } from '../../pages';
+import { HomePage, EMPage } from '../../pages';
 
 const Content = () => {
     return (
         <main>
             <Switch>
                 <Route path="/" exact component={HomePage} />
+                <Route path="/eumasters" component={EMPage} />
             </Switch>
         </main>
     )
---
+++ client/src/pages/EM/EM.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './EM.scss';
+
+const EMPage = () => {
+    return (
+        <div>EU Masters</div>
+    )
+}
+
+export default EMPage;
